bdd702609e0d50b04d6995d30a9ccecc8c5d5cff
Peter Walker <peter.walker@walmartlabs.com>
2020-02-07 16:05:13 -0800

    Added nvim auto install script

---
+++ nvim/install.sh
@@ -0,0 +1,7 @@
+echo "Downloading vim-plug"
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+echo "Make nvim look at our ~/.vimrc file"
+mkdir -p ~/.config/nvim
+cp ./init.vim ~/.config/nvim/init.vim
+
